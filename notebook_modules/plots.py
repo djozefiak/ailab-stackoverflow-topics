0718a7bf6ac2980c53be8dd3aa3148285993dfa4
David Jozefiak <35257958+djozefiak@users.noreply.github.com>
2020-05-27 23:01:12 +0200

    Add notebook for distribution analysis

--- notebook_modules/plots.py
+++ notebook_modules/plots.py
@@ -12,9 +12,16 @@ def make_tag_plot(topic, name, dataframe):
 
 def make_tag_plot_from_csv(topic, name):
 	dataframe = pd.read_csv(f"output/lists/{topic}-{name}.csv")
+	make_tag_plot(topic, name, dataframe)
+
+def make_frequency_plot(topic, name, dataframe):
 	plt.figure(figsize=(20,8))
 	plt.title(name)
 	plt.xticks(rotation="vertical")
 	sns.set(style="whitegrid")
-	sns.barplot(x="quarter", y=topic, data=dataframe)
+	sns.lineplot(x="tag", y=topic, sort=False, data=dataframe)
 	plt.savefig(f"output/plots/{topic}-{name}.png")
+
+def make_frequency_plot_from_csv(topic, name):
+	dataframe = pd.read_csv(f"output/lists/{topic}-{name}.csv")
+	make_frequency_plot(topic, name, dataframe)
