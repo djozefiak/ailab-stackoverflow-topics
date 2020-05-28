97750941b5cf345038c4ce079043f5dc8dd6d799
David Jozefiak <35257958+djozefiak@users.noreply.github.com>
2020-05-28 19:50:15 +0200

    Rework plotting, merge duplicate code

--- notebook_modules/plots.py
+++ notebook_modules/plots.py
@@ -2,26 +2,22 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def make_tag_plot(topic, name, dataframe):
-	plt.figure(figsize=(20,8))
-	plt.title(name)
-	plt.xticks(rotation="vertical")
+def setup():
 	sns.set(style="whitegrid")
-	sns.barplot(x="quarter", y=topic, data=dataframe)
-	plt.savefig(f"output/plots/{topic}-{name}.png")
+	plt.figure(figsize=(20,8))
+	plt.xticks(rotation="vertical")
 
-def make_tag_plot_from_csv(topic, name):
-	dataframe = pd.read_csv(f"output/lists/{topic}-{name}.csv")
-	make_tag_plot(topic, name, dataframe)
+def get_dataframe_from_csv(topic, name):
+	return pd.read_csv(f"output/lists/{topic}-{name}.csv")
+
+def make_tag_plot(topic, name, dataframe):
+	setup()
+	plt.title(name)
+	sns.barplot(x="quarter", y=topic, data=dataframe)
+	plt.savefig(f"output/plots/{topic}-{name}.png", bbox_inches="tight")
 
 def make_frequency_plot(topic, name, dataframe):
-	plt.figure(figsize=(20,8))
+	setup()
 	plt.title(name)
-	plt.xticks(rotation="vertical")
-	sns.set(style="whitegrid")
 	sns.barplot(x="tag", y=topic, data=dataframe)
-	plt.savefig(f"output/plots/{topic}-{name}.png")
-
-def make_frequency_plot_from_csv(topic, name):
-	dataframe = pd.read_csv(f"output/lists/{topic}-{name}.csv")
-	make_frequency_plot(topic, name, dataframe)
+	plt.savefig(f"output/plots/{topic}-{name}.png", bbox_inches="tight")
