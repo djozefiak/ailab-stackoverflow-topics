e197d684d2654988649dccd7c99045481f6d132c
David Jozefiak <35257958+djozefiak@users.noreply.github.com>
2020-05-28 21:10:34 +0200

    Add notebook and plots for distribution analysis

--- notebook_modules/plots.py
+++ notebook_modules/plots.py
@@ -7,6 +7,13 @@ def setup():
 	plt.figure(figsize=(20,8))
 	plt.xticks(rotation="vertical")
 
+def setup_dist():
+	sns.set(style="whitegrid")
+	plt.figure(figsize=(8,8))
+	ticks = [i / 10 for i in range(0, 11)]
+	plt.xticks(ticks)
+	plt.yticks(ticks)
+
 def get_dataframe_from_csv(topic, name):
 	return pd.read_csv(f"output/lists/{topic}-{name}.csv")
 
@@ -20,4 +27,10 @@ def make_frequency_plot(topic, name, dataframe):
 	setup()
 	plt.title(name)
 	sns.barplot(x="tag", y=topic, data=dataframe)
+	plt.savefig(f"output/plots/{topic}-{name}.png", bbox_inches="tight")
+
+def make_distribution_plot(topic, name, dataframe):
+	setup_dist()
+	plt.title(name)
+	sns.lineplot(x="percentage", y="share", data=dataframe)
 	plt.savefig(f"output/plots/{topic}-{name}.png", bbox_inches="tight")
