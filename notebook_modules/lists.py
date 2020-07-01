d45588f3eea0c484d78055a38d32a849465e3991
David Jozefiak <35257958+djozefiak@users.noreply.github.com>
2020-07-01 11:23:21 +0200

    Add functions for saving and loading large exports

--- notebook_modules/lists.py
+++ notebook_modules/lists.py
@@ -1,2 +1,11 @@
+import pandas as pd
+
+# keep old function to avoid breaking notebooks
 def make_list(topic, name, dataframe):
 	dataframe.to_csv(f"output/lists/{topic}-{name}.csv", index=False)
+
+def save_db(dataframe, topic, name, sep=";"):
+	dataframe.to_csv(f"output/db/{topic}-{name}.csv", index=False, sep=sep)
+
+def load_db(topic, name, sep=";"):
+	return pd.read_csv(f"output/db/{topic}-{name}.csv", sep=sep)
